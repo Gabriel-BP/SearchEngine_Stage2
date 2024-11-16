342a0c7d00aaf10e70743adba675dd070e71189c
raquelaq <raquel.almeida105@alu.ulpgc.es>
2024-11-16 15:28:40 +0000

    Query Engine Development

--- QueryEngine/src/main/java/es/ulpgc/Main.java
+++ QueryEngine/src/main/java/es/ulpgc/Main.java
@@ -1,11 +1,16 @@
 package es.ulpgc;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        InvertedIndex invertedIndex = new InvertedIndex();
+        invertedIndex.loadIndex("index_content.csv");
 
         System.out.println("Welcome to the Query Engine!");
         System.out.println("Please, write down your query:");
@@ -16,9 +21,25 @@ public class Main {
         QueryTokenizer tokenizer = new QueryTokenizer();
         List<String> tokens = tokenizer.tokenize(query);
 
-        System.out.println("Tokens generated:");
+        Set<String> finalResults = new HashSet<>();
+        boolean firstToken = true;
         for (String token : tokens) {
-            System.out.println(token);
+            Set<String> results = invertedIndex.search(token);
+            if (firstToken) {
+                finalResults.addAll(results);
+                firstToken = false;
+            } else {
+                finalResults.retainAll(results); // Intersección de resultados
+            }
+        }
+
+        if (finalResults.isEmpty()) {
+            System.out.println("No results found for the query.");
+        } else {
+            System.out.println("Documentos encontrados:");
+            for (String ebookNumber : finalResults) {
+                System.out.println("EbookNumber: " + ebookNumber);
+            }
         }
 
         scanner.close();
