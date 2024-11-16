9dd22cdade8f5691354c43a7ee7639eb28aefa75
raquelaq <raquel.almeida105@alu.ulpgc.es>
2024-11-16 13:50:16 +0000

    Query Engine Development

--- QueryEngine/src/main/java/es/ulpgc/Main.java
+++ QueryEngine/src/main/java/es/ulpgc/Main.java
@@ -1,7 +1,26 @@
 package es.ulpgc;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Query Engine!");
+        System.out.println("Please, write down your query:");
+
+        String userInput = scanner.nextLine();
+        Query query = new Query(userInput);
+
+        QueryTokenizer tokenizer = new QueryTokenizer();
+        List<String> tokens = tokenizer.tokenize(query);
+
+        System.out.println("Tokens generated:");
+        for (String token : tokens) {
+            System.out.println(token);
+        }
+
+        scanner.close();
     }
 }
